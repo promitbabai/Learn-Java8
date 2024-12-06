de1ffdab89f9e3f6c4d62afc7b0cbe70e563c5ad
Promit Majumder <62225575+promitbabai@users.noreply.github.com>
2024-12-07 00:05:46 +0530

    Update StreamAPITerminalOperations.java
    Remaining Terminal Operations added

--- src/com/learn/streamapi/StreamAPITerminalOperations.java
+++ src/com/learn/streamapi/StreamAPITerminalOperations.java
@@ -5,6 +5,16 @@ package com.learn.streamapi;
  *      1. collect() - used to return the result of the intermediate operations performed on the stream.
  *      2. forEach() - used to iterate through every element of the stream.
  *      3. reduce()  - reduce method is used to reduce the elements of a stream to a single value. The reduce method takes a BinaryOperator as a parameter.
+ *forEachOrdered()
+ *findAny()
+ *findFirst()
+ *toArray()
+ *count()
+ *min()
+ *max()
+ *anyMatch()
+ *allMatch()
+ *noneMatch()
  *
  * Note: Intermediate Operations are running based on the concept of Lazy Evaluation, which ensures that every method returns a fixed value(Terminal operation) before moving to the next method.
  *
